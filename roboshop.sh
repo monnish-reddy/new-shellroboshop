b4d0b3e8099268fce8e1af1d376710e00aa6e6b4
monnish-reddy <peta.monnish@gmail.com>
2025-09-04 16:45:43 -0400

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-02f8a8f87c9497fcd"
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" 
-"dispatch" "frontend")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z0559090BH4WWPB2ISUM"
 DOMAIN_NAME="monnish.com"
 
 for instance in ${INSTANCES[@]}
 do
-   
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f \
-     --instance-type t2.micro \
-     --security-group-ids sg-02f8a8f87c9497fcd \
-     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-     --query "Instances[0].InstanceId" --output text)
-    
-    if [ $instance != "frontend" ]
-    then 
+    if [ "$instance" == "frontend" ]; then
+        INSTANCE_ID=$(aws ec2 run-instances \
+            --image-id $AMI_ID \
+            --instance-type t2.micro \
+            --security-group-ids $SG_ID \
+            --associate-public-ip-address \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+            --query "Instances[0].InstanceId" --output text)
+    else
+        INSTANCE_ID=$(aws ec2 run-instances \
+            --image-id $AMI_ID \
+            --instance-type t2.micro \
+            --security-group-ids $SG_ID \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+            --query "Instances[0].InstanceId" --output text)
+    fi
+
+    # Wait until instance is running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+    # Fetch IP (Public for frontend, Private for others)
+    if [ "$instance" == "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+            --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-        --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     fi
-    echo  "$instance IP ADDRESS : $IP"
 
+    echo "$instance IP ADDRESS : $IP"
+
+    # Update Route53 DNS
     aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-    "Comment": "Testing creating a record set"
-    ,"Changes": [{
-      "Action"              : ""
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$instance'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "' $IP'"
+      --hosted-zone-id $ZONE_ID \
+      --change-batch "{
+        \"Comment\": \"Add DNS record for $instance\",
+        \"Changes\": [{
+          \"Action\": \"UPSERT\",
+          \"ResourceRecordSet\": {
+            \"Name\": \"$instance.$DOMAIN_NAME\",
+            \"Type\": \"A\",
+            \"TTL\": 60,
+            \"ResourceRecords\": [{\"Value\": \"$IP\"}]
+          }
         }]
-      }
-    }]
-  }'
-
-    
-
+      }"
 done
