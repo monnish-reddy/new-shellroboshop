73e72a577639c48651a1daee3a828178c4f3ef0a
monnish-reddy <peta.monnish@gmail.com>
2025-09-04 16:13:49 -0400

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -12,10 +12,11 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f \
      --instance-type t2.micro \
      --security-group-ids sg-02f8a8f87c9497fcd \
-     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" \
+     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
      --query "Instances[0].InstanceId" --output text)
-     if [ $instance != "frontend" ]
-     then 
+    
+    if [ $instance != "frontend" ]
+    then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
         --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
